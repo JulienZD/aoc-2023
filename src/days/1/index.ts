f4666fe9faa2ccaea19ecb577ab22d739247ca09
Julien Zapata Duque <jzapata@live.nl>
2023-12-01 08:02:17 +0100

    chore: initial commit

---
+++ src/days/1/index.ts
@@ -0,0 +1,5 @@
+import { Solver } from '../../solution.js';
+
+export const part1: Solver = (input) => {
+  return input.length;
+};
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { getDayInput } from './util/get-solution.js';
+
+const args = process.argv.slice(2);
+const dayNumber = String(args[0] || new Date().getDate());
+
+const dayDirectory = path.join(fileURLToPath(import.meta.url), '../', 'days', dayNumber);
+
+const input = await getDayInput(+dayNumber, dayDirectory);
+
+const { part1, part2 } = await import(path.join(dayDirectory, 'index.ts'));
+
+if (part1) {
+  console.log(`Day ${dayNumber} - Part 1:`, part1(input));
+}
+
+if (part2) {
+  console.log(`Day ${dayNumber} - Part 2:`, part2(input));
+}
