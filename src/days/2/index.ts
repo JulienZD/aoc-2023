ce44723c936084e87764130ac5bab0e6d5949b75
Julien Zapata Duque <jzapata@live.nl>
2023-12-03 12:04:07 +0100

    feat: solve day 2 part 2

--- src/days/2/index.ts
+++ src/days/2/index.ts
@@ -45,15 +45,42 @@ function isValidGame(sets: readonly CubeSet[]): boolean {
 }
 
 export const part2: Solver = (input) => {
-  return input.length;
+  return input
+    .map((line) => {
+      const [_game, bagRetrievals] = line.replaceAll(' ', '').split(':') as [string, string];
+
+      const sets = bagRetrievals.split(';') as CubeSet[];
+
+      return getCubedTotalForGame(sets);
+    })
+    .reduce((accum, curr) => accum + curr, 0);
 };
 
-const example = [
-  'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
-  'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
-  'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
-  'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
-  'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
-];
+function getCubedTotalForGame(sets: readonly CubeSet[]): number {
+  const countsPerColor = sets.map((set) => {
+    return Object.fromEntries(
+      COLORS.map((color) => {
+        const count = Number(set.match(new RegExp(`(\\d+)${color}`))?.at(1) || 0);
 
-console.log(part1(example));
+        return [color, count] as const;
+      })
+    ) as Record<Color, number>;
+  });
+
+  const maxCountsRequired = countsPerColor.reduce(
+    (totals, setResults) => {
+      COLORS.forEach((color) => {
+        if (setResults[color] > totals[color]) {
+          totals[color] = setResults[color];
+        }
+      });
+
+      return totals;
+    },
+    Object.fromEntries(COLORS.map((color) => [color, Number.MIN_SAFE_INTEGER])) as Record<Color, number>
+  );
+
+  return Object.values(maxCountsRequired).reduce((total, count) => {
+    return count === 0 ? total : total * count;
+  }, 1);
+}
