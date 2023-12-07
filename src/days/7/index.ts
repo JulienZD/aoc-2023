e7a483efe50765f199c69df13fd7f389ad0ca96a
Julien Zapata Duque <jzapata@live.nl>
2023-12-07 19:28:26 +0100

    feat: solve day 7 part 2

--- src/days/7/index.ts
+++ src/days/7/index.ts
@@ -12,7 +12,34 @@ export const part1: Solver = (input) => {
       const scoreB = scoreHand(b);
 
       if (scoreA === scoreB) {
-        return determineWinner(handOne, handTwo);
+        return determineWinner(handOne, handTwo, CARDS);
+      }
+
+      return scoreA > scoreB ? Sort.ASC : Sort.DESC;
+    })
+    .map(([_, bid], index) => {
+      const rank = index + 1;
+
+      return Number(bid) * rank;
+    })
+    .reduce((a, b) => a + b);
+};
+
+export const part2: Solver = (input) => {
+  const hands = input.map((line) => line.split(' '));
+
+  const cards = { ...CARDS, [JOKER]: 1 };
+
+  return hands
+    .sort(([handOne], [handTwo]) => {
+      const a = handOne?.split('').sort().join('');
+      const b = handTwo?.split('').sort().join('');
+
+      const scoreA = scoreHandPartTwo(a);
+      const scoreB = scoreHandPartTwo(b);
+
+      if (scoreA === scoreB) {
+        return determineWinner(handOne, handTwo, cards);
       }
 
       return scoreA > scoreB ? Sort.ASC : Sort.DESC;
@@ -56,6 +83,8 @@ const CARDS = {
   2: 2,
 } as const;
 
+const JOKER = 'J';
+
 function scoreHand(hand: [string, string, string, string, string]): number {
   const [a, b, c, d, e] = hand;
 
@@ -93,16 +122,91 @@ function scoreHand(hand: [string, string, string, string, string]): number {
   return Score.ONE_PAIR;
 }
 
-function determineWinner(handOne: string, handTwo: string): Sort {
-  for (let i = 0; i < handOne.length; i++) {
-    const cardA = CARDS[handOne[i]];
-    const cardB = CARDS[handTwo[i]];
+// This thing is just lovely
+function scoreHandPartTwo(hand: [string, string, string, string, string]): number {
+  const [a, b, c, d, e] = hand;
 
-    if (cardA === cardB) {
+  const amountOfJokers = [...hand].filter((card) => card === JOKER).length;
+
+  // Five of a kind
+  if (a === b && a === c && a === d && a === e) {
+    return Score.FIVE_OF_A_KIND;
+  }
+
+  // Four of a kind
+  if ((a === b && a === c && a === d) || (b === c && b === d && b === e)) {
+    if (amountOfJokers === 1 || amountOfJokers === 4) {
+      return Score.FIVE_OF_A_KIND;
+    }
+    return Score.FOUR_OF_A_KIND;
+  }
+
+  // Full house
+  if ((a === b && a === c && d === e) || (a === b && c === d && c === e)) {
+    if (amountOfJokers === 1) {
+      return Score.FOUR_OF_A_KIND;
+    }
+
+    if (amountOfJokers === 2 || amountOfJokers === 3) {
+      return Score.FIVE_OF_A_KIND;
+    }
+
+    return Score.FULL_HOUSE;
+  }
+
+  // Three of a kind
+  if ((a === b && a === c) || (b === c && b === d) || (c === d && c === e)) {
+    if (amountOfJokers === 1 || amountOfJokers === 3) {
+      return Score.FOUR_OF_A_KIND;
+    }
+
+    if (amountOfJokers === 2) {
+      return Score.FIVE_OF_A_KIND;
+    }
+
+    return Score.THREE_OF_A_KIND;
+  }
+
+  // Two pair
+  if ((a === b && c === d) || (b === c && d === e) || (a === b && d === e)) {
+    if (amountOfJokers === 1) {
+      return Score.FULL_HOUSE;
+    }
+
+    if (amountOfJokers === 2) {
+      return Score.FOUR_OF_A_KIND;
+    }
+
+    return Score.TWO_PAIR;
+  }
+
+  // High card
+  if (new Set(hand).size === hand.length) {
+    if (amountOfJokers === 1) {
+      return Score.ONE_PAIR;
+    }
+
+    return Score.HIGH_CARD;
+  }
+
+  if (amountOfJokers === 1 || amountOfJokers === 2) {
+    return Score.THREE_OF_A_KIND;
+  }
+
+  // One pair
+  return Score.ONE_PAIR;
+}
+
+function determineWinner(handOne: string, handTwo: string, cards: Record<string, number>): Sort {
+  for (let i = 0; i < handOne.length; i++) {
+    const scoreA = cards[handOne[i]];
+    const scoreB = cards[handTwo[i]];
+
+    if (scoreA === scoreB) {
       continue;
     }
 
-    if (cardA > cardB) {
+    if (scoreA > scoreB) {
       return Sort.ASC;
     }
 
