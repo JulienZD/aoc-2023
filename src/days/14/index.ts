c49a8f4101ebcabe59495a26b052c15878ac378f
Julien Zapata Duque <jzapata@live.nl>
2023-12-14 21:39:00 +0100

    blehhh

--- src/days/14/index.ts
+++ src/days/14/index.ts
@@ -11,6 +11,12 @@ type Tile = (typeof Tile)[keyof typeof Tile];
 export const part1: Solver = (input) => {
   const tiltedGrid = tiltGrid(input.map((row) => row.split('')) as Grid);
 
+  return calculateLoad(tiltedGrid);
+};
+
+export const part2: Solver = (input) => {
+  const tiltedGrid = tiltGrid2(input.map((row) => row.split('')) as Grid, 1_000_000_000);
+
   return tiltedGrid.reduce((total, row, rowIndex) => {
     const load = tiltedGrid.length - rowIndex;
 
@@ -41,6 +47,131 @@ function tiltGrid(_grid: Grid): Grid {
   return grid;
 }
 
+function calculateLoad(grid: Grid): number {
+  return grid.reduce((total, row, rowIndex) => {
+    const load = grid.length - rowIndex;
+
+    return total + row.reduce((rowTotal, tile) => rowTotal + (tile === Tile.ROUNDED_ROCK ? load : 0), 0);
+  }, 0);
+}
+
+const DIRECTIONS = ['N', 'W', 'S', 'E'] as const;
+type Direction = (typeof DIRECTIONS)[number];
+
+function tiltGrid2(_grid: Grid, maxCycles = 1): Grid {
+  let grid = _grid;
+
+  const cache = new Map<string, number>();
+
+  for (let cycle = 1; cycle <= maxCycles; cycle++) {
+    // if (cycle % 100 === 0) {
+    console.log(cycle);
+    // }
+
+    if (cycle === 12) {
+      break;
+    }
+
+    const gridKey = JSON.stringify(grid);
+
+    if (cache.has(gridKey)) {
+      console.log('dupe at1', cycle);
+      continue;
+    }
+
+    let gridToProcess = grid;
+
+    for (const direction of DIRECTIONS) {
+      logGrid(gridToProcess);
+
+      if (cache.has(JSON.stringify(gridToProcess))) {
+        console.log('dupe at 2', cycle, direction);
+        continue;
+      }
+
+      let hasMoved = true;
+
+      while (hasMoved) {
+        let didMove = false;
+
+        const withMovables = markMovableTiles(gridToProcess);
+
+        const { tiltedGrid, movements } = applyMovements(withMovables);
+
+        gridToProcess = tiltedGrid;
+
+        if (!didMove) {
+          didMove = movements > 0;
+        }
+
+        hasMoved = didMove;
+      }
+
+      cache.set(JSON.stringify(gridToProcess), calculateLoad(gridToProcess));
+
+      gridToProcess = rotateGrid(grid);
+    }
+
+    grid = gridToProcess;
+  }
+
+  return grid;
+}
+
+function cycle(_grid: Grid): Grid {
+  let grid = _grid;
+  for (let i = 0; i < 4; i++) {
+    grid = rotateGrid(grid);
+    let hasMoved = true;
+
+    while (hasMoved) {
+      let didMove = false;
+
+      const withMovables = markMovableTiles(grid);
+
+      const { tiltedGrid, movements } = applyMovements(withMovables);
+
+      grid = tiltedGrid;
+
+      if (!didMove) {
+        didMove = movements > 0;
+      }
+
+      hasMoved = didMove;
+    }
+  }
+
+  return grid;
+}
+
+function rotateGrid(grid: Grid): Grid {
+  // return grid[0]!.map((_val, index) => grid.map((row) => row[index]!).reverse());
+  return grid[0]!.map((_val, index) => grid.map((row) => row[row.length - 1 - index]!));
+  // switch (direction) {
+  //   case 'N':
+  //     return grid.map((row) => row.slice());
+  //   case 'S':
+  //     return grid.map((row) => row.slice().reverse()).reverse();
+  //   case 'E':
+
+  //   case 'W':
+  //     return grid[0]!.map((_val, index) => grid.map((row) => row[row.length - 1 - index]!));
+  // }
+}
+
+function oppositeDirection(direction: Direction): Direction {
+  switch (direction) {
+    case 'N':
+      return 'S';
+    case 'S':
+      return 'N';
+    case 'W':
+      return 'E';
+    case 'E':
+      return 'W';
+  }
+}
+
 function logGrid(grid: Grid) {
   console.log('---  ---');
   console.log(grid.map((row) => row.join('')).join('\n'));
@@ -50,13 +181,13 @@ function logGrid(grid: Grid) {
 type GridWithMovables = ReadonlyArray<ReadonlyArray<{ tile: Tile; movable: boolean }>>;
 
 function markMovableTiles(grid: Grid): GridWithMovables {
-  const tiltedGrid = grid.map((row) => row.slice());
+  const gridCopy = grid.map((row) => row.slice());
 
   const movables: Array<[number, number]> = [];
 
-  for (let rowIndex = 0; rowIndex < tiltedGrid.length - 1; rowIndex++) {
-    const row = tiltedGrid[rowIndex]!;
-    const nextRow = tiltedGrid[rowIndex + 1]!;
+  for (let rowIndex = 0; rowIndex < gridCopy.length - 1; rowIndex++) {
+    const row = gridCopy[rowIndex]!;
+    const nextRow = gridCopy[rowIndex + 1]!;
 
     for (let tileIndex = 0; tileIndex < row.length; tileIndex++) {
       const tile = nextRow[tileIndex]!;
@@ -87,7 +218,7 @@ function markMovableTiles(grid: Grid): GridWithMovables {
     }
   }
 
-  return tiltedGrid.map((row, rowIndex) => {
+  return gridCopy.map((row, rowIndex) => {
     return row.map((cell, colIndex) => {
       const isMovable = !!movables.find(([y, x]) => y === rowIndex && x === colIndex);
       return {
@@ -137,4 +268,4 @@ const example = [
   '#OO..#....',
 ];
 
-console.log(part1(example));
+console.log(part2(example));
