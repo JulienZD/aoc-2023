9006e96aff096878ff7e01a952e6f2456804511e
Julien Zapata Duque <jzapata@live.nl>
2023-12-16 12:49:20 +0100

    feat: solve day 15 part 2

--- src/days/15/index.ts
+++ src/days/15/index.ts
@@ -19,3 +19,55 @@ function hash(input: string): number {
     return currentValue;
   }, 0);
 }
+
+export const part2: Solver = (input) => {
+  const strings = input[0]!.split(',');
+
+  const boxes = fillBoxesWithLenses(strings);
+
+  return [...boxes.entries()]
+    .flatMap(([boxIndex, lenses]) => {
+      return lenses.map(({ focalLength }, lensIndex) => {
+        const a = boxIndex + 1;
+        const b = lensIndex + 1;
+        const c = focalLength;
+
+        return a * b * c;
+      });
+    })
+    .reduce((a, b) => a + b);
+};
+
+function fillBoxesWithLenses(steps: ReadonlyArray<string>) {
+  const boxes = new Map<number, { label: string; focalLength: number }[]>();
+
+  for (const step of steps) {
+    // Wrap the delimiter in a group to include it in the result
+    const [label, operation, focalLength] = step.split(/([=-])/) as [string, string, `${number}` | ''];
+
+    const key = hash(label);
+
+    if (!boxes.has(key)) {
+      boxes.set(key, []);
+    }
+
+    const lenses = boxes.get(key)!;
+
+    const labelIndex = lenses.findIndex((item) => item.label === label);
+
+    if (operation === '=') {
+      const item = { label, focalLength: Number(focalLength) };
+      if (labelIndex !== -1) {
+        lenses.splice(labelIndex, 1, item);
+      } else {
+        lenses.push(item);
+      }
+    } else if (operation === '-') {
+      if (labelIndex !== -1) {
+        lenses.splice(labelIndex, 1);
+      }
+    }
+  }
+
+  return boxes;
+}
